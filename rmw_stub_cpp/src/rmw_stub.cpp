8af848037d7e76d86abae8b1e7364f15723b803c
Mauro Passerino <mpasserino@irobot.com>
2022-11-21 17:54:13 -0300

    Add backtraces

--- rmw_stub_cpp/src/rmw_stub.cpp
+++ rmw_stub_cpp/src/rmw_stub.cpp
@@ -50,6 +50,10 @@
 #include "rmw_stub_cpp/stub_service.hpp"
 #include "rmw_stub_cpp/stub_subscription.hpp"
 
+// Backtrace
+#include <execinfo.h>
+#include <stdio.h>
+
 using namespace std::literals::chrono_literals;
 
 using rmw_dds_common::msg::ParticipantEntitiesInfo;
@@ -1155,8 +1159,17 @@ rmw_ret_t rmw_wait(
   (void)wait_set;
   (void)wait_timeout;
 
-  RMW_SET_ERROR_MSG("rmw_wait not implemented");
-  return RMW_RET_UNSUPPORTED;
+  // Get backtrace
+  void * callstack[128];
+  int i, frames = backtrace(callstack, 128);
+  char** strs = backtrace_symbols(callstack, frames);
+  for (i = 0; i < frames; ++i) {
+    printf("%s\n", strs[i]);
+  }
+  free(strs);
+
+  // RMW_SET_ERROR_MSG("rmw_wait not implemented");
+  return RMW_RET_OK;
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////
@@ -1213,6 +1226,15 @@ rmw_ret_t rmw_send_request(
   (void)client;
   (void)ros_request;
   (void)sequence_id;
+
+  // Get backtrace
+  void * callstack[128];
+  int i, frames = backtrace(callstack, 128);
+  char** strs = backtrace_symbols(callstack, frames);
+  for (i = 0; i < frames; ++i) {
+    printf("%s\n", strs[i]);
+  }
+  free(strs);
 
   RMW_SET_ERROR_MSG("rmw_send_response not implemented");
   return RMW_RET_UNSUPPORTED;
